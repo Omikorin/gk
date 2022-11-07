96d02f2e3d34fcb0de7357fa496ae90ec5af5062
Omikorin <michal.k2001@gmail.com>
2022-11-07 18:01:33 +0100

    add lab1

--- lab4/lab1.py
+++ lab4/lab1.py
@@ -10,11 +10,14 @@ from OpenGL.GLU import *
 viewer = [0.0, 0.0, 10.0]
 
 theta = 0.0
+phi = 0.0
 pix2angle = 1.0
 
 left_mouse_button_pressed = 0
 mouse_x_pos_old = 0
 delta_x = 0
+mouse_y_pos_old = 0
+delta_y = 0
 
 
 def startup():
@@ -82,6 +85,7 @@ def example_object():
 
 def render(time):
     global theta
+    global phi
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
@@ -91,8 +95,10 @@ def render(time):
 
     if left_mouse_button_pressed:
         theta += delta_x * pix2angle
+        phi += delta_y * pix2angle
 
     glRotatef(theta, 0.0, 1.0, 0.0)
+    glRotatef(phi, 1.0, 0.0, 0.0)
 
     axes()
     example_object()
@@ -127,8 +133,14 @@ def mouse_motion_callback(window, x_pos, y_pos):
     global delta_x
     global mouse_x_pos_old
 
+    global delta_y
+    global mouse_y_pos_old
+
     delta_x = x_pos - mouse_x_pos_old
     mouse_x_pos_old = x_pos
+
+    delta_y = y_pos - mouse_y_pos_old
+    mouse_y_pos_old = y_pos
 
 
 def mouse_button_callback(window, button, action, mods):
